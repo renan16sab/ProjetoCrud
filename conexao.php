190c0d1fc044baaa610adc98281f8d5dc6bfbd8a
Renan Araújo <renan-ano10@hotmail.com>
2018-03-06 00:23:22 -0300

    Criação da conexão com o Database

--- conexao.php
+++ conexao.php
@@ -1,4 +1,15 @@
 <?php
-echo "Ola mundo";
+$username = 'root';
+$password = '';
+
+try {
+    $dbconn = new PDO('mysql:host=localhost;dbname=linkizi', $username, $password);
+} catch (PDOException $e) {
+
+    print "Error!: " . $e->getMessage() . "<br/>";
+
+    die();
+
+} 
 
 ?>
